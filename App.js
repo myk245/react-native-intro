d9146e0b2a34051ff059e71da69743d1f90545c5
myk245 <myk245@nyu.edu>
2021-03-24 00:59:41 -0400

    add core components

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app.')
 
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text has changed.')}/>
-      <StatusBar style="auto" />
+    <View>
+      <View>
+        <TextInput />
+        <Button title="ADD"/>
+      </View>
+
+      <View>
+        
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
