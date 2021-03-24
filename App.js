1a96fb420e5146b21d7b14f5bfe1527e44bb8497
myk245 <myk245@nyu.edu>
2021-03-24 01:12:24 -0400

    add styling

--- App.js
+++ App.js
@@ -4,14 +4,27 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
 
   return (
-    <View>
-      <View>
-        <TextInput />
+    <View style={{
+      padding: 50
+    }}>
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <TextInput
+          style={{
+            width: 200, 
+            borderColor: 'gray',
+            borderWidth: 1,
+            padding: 10
+          }}
+          placeholder="Course Goal" />
         <Button title="ADD"/>
       </View>
 
       <View>
-        
+
       </View>
     </View>
   );
