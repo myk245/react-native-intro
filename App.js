d0c8be4545114a04f1e4755a78c73f245eb89fdc
myk245 <myk245@nyu.edu>
2021-03-24 23:31:52 -0400

    handle user input and button press

--- App.js
+++ App.js
@@ -2,14 +2,29 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText)
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal)
+  }
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder="Course Goal" />
-        <Button title="ADD"/>
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button
+          title="ADD"
+          onPress={addGoalHandler}
+        />
       </View>
 
       <View>
